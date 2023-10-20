38401a41f78b9b2c13643db14070d39bc6f31dae
FranciscoheronildoCleveriano <francisco.santos@cleversystems.com.br>
2023-10-20 09:53:05 -0300

    [installer-1] - Criando atualizador

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { app, shell } from 'electron';
-import { resolve, join } from 'path';
-
-const path_pesquisa = join(process.resourcesPath, 'pesquisa.exe');
-
-app.whenReady().then(() => {
- shell.openPath(resolve(path_pesquisa));
-})
