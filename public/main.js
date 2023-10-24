71b3e640f36c0cf1c1ac78ecea8fb6de69969848
FranciscoheronildoCleveriano <francisco.santos@cleversystems.com.br>
2023-10-24 11:46:31 -0300

    [installer-2]-Atualizador

--- public/main.js
+++ public/main.js
@@ -1,8 +1,82 @@
-"use strict";var _electron = require('electron');
-var _path = require('path');
+"use strict";const {
+	app,
+	// BrowserWindow,
+	ipcMain,
+} = require("electron");
+const {
+	autoUpdater,
+} = require("electron-updater");
+const { resolve, join } = require("path");
 
-const path_pesquisa = _path.join.call(void 0, process.resourcesPath, 'pesquisa.exe');
+let mainWindow;
 
-_electron.app.whenReady().then(() => {
- _electron.shell.openPath(_path.resolve.call(void 0, path_pesquisa));
-})
+if (process.env.NODE_ENV === "development") {
+	autoUpdater.autoDownload = false;
+	autoUpdater.autoInstallOnAppQuit = false;
+	setInterval(() => {
+		autoUpdater.checkForUpdates();
+	}, 30000);
+}
+
+const path = join(
+	process.resourcesPath,
+	"pesquisa.exe"
+);
+
+function createWindow() {
+	// mainWindow = new BrowserWindow({
+	// 	width: 800,
+	// 	height: 600,
+	// 	webPreferences: {
+	// 		nodeIntegration: true,
+	// 	},
+	// });
+	// mainWindow.loadFile("index.html");
+	// mainWindow.on("closed", function () {
+	// 	mainWindow = null;
+	// });
+	app.whenReady().then(() => {
+		shell.openPath(resolve(path));
+	});
+	mainWindow.once("ready-to-show", () => {
+		setInterval(() => {
+			autoUpdater.checkForUpdatesAndNotify();
+		}, 30000);
+	});
+}
+
+// app.on("ready", () => {
+// 	createWindow();
+// });
+
+app.on("window-all-closed", function () {
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
+});
+
+app.on("activate", function () {
+	if (mainWindow === null) {
+		createWindow();
+	}
+});
+
+ipcMain.on("app_version", (event) => {
+	event.sender.send("app_version", {
+		version: app.getVersion(),
+	});
+});
+
+autoUpdater.on("update-available", () => {
+	mainWindow.webContents.send("update_available");
+});
+
+autoUpdater.on("update-downloaded", () => {
+	mainWindow.webContents.send(
+		"update_downloaded"
+	);
+});
+
+ipcMain.on("restart_app", () => {
+	autoUpdater.quitAndInstall();
+});
